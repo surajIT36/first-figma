f58489713d820b0d2d23a806dd38c920974327d9
surajkumarsingh <surajkumarsingh392@gmail.com>
2022-09-01 02:10:47 +0530

    signup page clear

--- pages/signup.js
+++ pages/signup.js
@@ -1,6 +1,7 @@
 import Signupheader from '../components/signupheader';
 import Form from 'react-bootstrap/Form';
 import styles from '../styles/signup.module.css';
+import Button from 'react-bootstrap/Button';
 import JoinNewsLetter from '../components/joinnewsLetter';
 import Footer from '../components/footer';
 
@@ -38,11 +39,35 @@ const signup = () => {
                         </Form.Group>
                     </div>
 
-                    <div >
-                    <Form.Group className="mb-5" controlId="formBasicCheckbox">
-                        <Form.Check type="checkbox"  />                       
-                        <span>By signing in i agree with <span style={{color:"#F170D3"}} >terms and condition</span></span>                      
+                    <div style={{display:"flex",padding:"30px",width:"393px"}} >
+                    <Form.Group controlId="formBasicCheckbox">
+                        <Form.Check type="checkbox"  /> 
+                        <span style={{position:"relative",bottom:"30px",left:"37px"}}>By signing in i agree with <span style={{color:"#F170D3"}} >terms and condition</span></span> 
                     </Form.Group>
+                    </div>
+
+                    <div style={{textAlign:"center",fontFamily: "Poppins",height:"69px",position:"relative",top: "-9px"}}>
+                    <Button variant="primary"
+                    style={{    
+                        background: "#F170D3",
+                        border: "none",
+                        width: "133px",
+                        borderRadius: "10px",
+                        height: "34px",     
+                        fontWeight: "500",
+                        fontSize: "20px",
+                        lineHeight: "23px",
+                        color: "#161622"}}
+                    >SIGN UP</Button>{' '}
+                    <span style={{padding:"18px"}}>OR</span>
+                    <Button variant="secondary"
+                    style={{    
+                        background: "none",
+                        border: "none",
+                        fontWeight: "400",
+                        fontSize: "20px",
+                        color: "#161622"}}
+                    >LOG IN</Button>{' '}
                     </div>
                    
 
