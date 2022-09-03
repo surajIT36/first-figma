93b587e3373b9f5696d17df47a2b88f81aad9452
surajkumarsingh <surajkumarsingh392@gmail.com>
2022-09-04 02:52:41 +0530

    blog details complete

--- components/blogdetails.js
+++ components/blogdetails.js
@@ -2,21 +2,27 @@ import NavBar from '../components/Navbar';
 import styles from '../styles/blogdetails.module.css';
 import JoinNewsLetter from '../components/joinnewsLetter';
 import Footer from '../components/footer';
-
+import Image from 'next/image';
+import slide from '../public/slide-1.png';
 
 
 const blogdetails = () => {
-    return(
+    return (
         <>
-        <NavBar />
-        <div className={styles.container}>
+            <NavBar />
+            <div className={styles.container}>
                 <div className={styles.containerBody}>
                     <h2 style={{ fontWeight: "700", fontSize: "24px", paddingBottom: "20px", color: "#161622" }}>Blog</h2>
                     <div>
-                        <img src='/slide-1.png' style={{width:"316px"}} />
+                        <Image
+                            src={slide}
+                            alt="Picture of the author"
+                        />
                     </div>
-                    <h4 style={{position:"relative",top:"12px",fontWeight: "700",fontSize: "16px",
-                    lineHeight: "24px",color: "#000000"}}
+                    <h4 style={{
+                        position: "relative", top: "12px", fontWeight: "700", fontSize: "16px",
+                        lineHeight: "24px", color: "#000000"
+                    }}
                     >The point of using Lorem Ipsum is that it has amore-or-less normal.
                     </h4>
 
@@ -38,17 +44,17 @@ const blogdetails = () => {
                             here, content here, making it
                             look like readable English.
                         </p>
-                        <p style={{color:"#F170D3",fontWeight: "500"}}>Shubham</p>  
-                        <p style={{color:"#F170D3",fontWeight: "500"}}> 9th sept 2022</p>
-                    </div>  
-                    
+                        <p style={{ color: "#F170D3", fontWeight: "500" }}>Shubham</p>
+                        <p style={{ color: "#F170D3", fontWeight: "500" }}> 9th sept 2022</p>
+                    </div>
 
-                     
+
+
+                </div>
             </div>
-        </div>
 
-        <JoinNewsLetter />
-        <Footer />
+            <JoinNewsLetter />
+            <Footer />
         </>
     );
 }
